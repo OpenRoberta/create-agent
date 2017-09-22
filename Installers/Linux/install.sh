714e9b73a4c2fd48d5fff7ab7b16e3a449d78354
Artem Vinokurov <artem.vinokurov@inf.h-brs.de>
2017-09-22 12:47:32 +0200

    OpenRoberta/robertalab#602 put a safeguard from running as root and made output more informative

--- Installers/Linux/install.sh
+++ Installers/Linux/install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+if [[ $EUID == 0 ]]
+  then echo "Please run this script as regular user not as root"
+  exit
+fi
+
 version="0.0.1"
 user=$(whoami)
 dir="/home/$user/ORCreateAgent"
 desktop_file="/home/$user/.local/share/applications/ORCreateAgent.desktop"
+
+echo "Creating menu entry..."
 
 echo "[Desktop Entry]" > $desktop_file
 echo "Version=$version" >> $desktop_file
@@ -18,10 +25,19 @@ echo "Categories=Application;Development;Internet;Education;" >> $desktop_file
 
 chmod u+x $desktop_file
 
+echo "Creating desktop shortcut..."
+
+cp $desktop_file /home/$user/Desktop
+
+echo "Copying data to $dir..."
+
 mkdir $dir
 cp arduino-create-agent $dir
 cp config.ini $dir
 cp OR.png $dir
 
+echo "Copying tools to /home/$user/.arduino-create..."
 cp -r .arduino-create /home/$user/
 echo "{\"apilevel\":\"v1\",\"avrdude\":\"/home/$user/.arduino-create/arduino/avrdude/6.3.0-arduino9\",\"avrdude-6.3.0-arduino9\":\"/home/$user/.arduino-create/arduino/avrdude/6.3.0-arduino9\"}" > /home/$user/.arduino-create/installed.json
+
+echo "Done!"
